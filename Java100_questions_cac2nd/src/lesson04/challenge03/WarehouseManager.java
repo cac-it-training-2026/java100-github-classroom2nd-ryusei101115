7f08b168b995557b1b6e61d3b57da3e4e462af88
ochiairy <ochiairy@C010423055.zipangu.cac.co.jp>
2026-04-20 16:52:44 +0900

    lesson04のchallenge03完了しました。

--- Java100_questions_cac2nd/src/lesson04/challenge03/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge03/WarehouseManager.java
@@ -92,15 +92,18 @@ public class WarehouseManager {
 
 		System.out.print("要素数を入れてください＞");
 
-
 		//ここに入力処理を記述する
-
+		String yousonum = br.readLine();
+		int youso = Integer.parseInt(yousonum);
 
 		//ここに配列宣言を記述する
-
+		String[] strArray = new String[youso];
 
 		//ここに値の入力+代入処理を記述する（for文）
-
+		for (int i = 0; i < strArray.length; i++) {
+			System.out.print("\n代入する値を入れてください＞");
+			strArray[i] = br.readLine();
+		}
 
 		System.out.println("\nYさん：");
 		System.out.println("...出来ました。\n");
@@ -110,9 +113,10 @@ public class WarehouseManager {
 
 		System.out.println("Yさん：");
 
-
 		//ここに配列の値の出力処理を記述する（for文）
-
+		for (int i = 0; i < strArray.length; i++) {
+			System.out.println(strArray[i]);
+		}
 
 		System.out.println("です。\n");
 
